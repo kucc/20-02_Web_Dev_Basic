605b308a7cdab9682a68e85e4157a57550c5c1b0
irin-kim <irenek91@korea.ac.kr>
2020-10-10 23:59:41 +0900

    week 03 HW 1010 focusevent added

---
+++ week_03/김이린/1-1
@@ -0,0 +1,2 @@
+//1-1. Hello World!를 출력하시오.
+console.log("Hello World!");
---
+++ week_03/김이린/script.js
@@ -0,0 +1,4 @@
+function focusgreen() {
+  let inputbox = document.getElementById("inputbox");
+  inputbox.style.border = "1px solid #03b853";
+}
