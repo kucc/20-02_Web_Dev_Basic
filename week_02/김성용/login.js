e5ef55b1232332c2e38d5e0c55266342b9c7e2f7
SeongYong-Kim <tjddyd1592@daum.net>
2020-11-07 23:23:16 +0900

    summit3

--- week_02/김성용/login.js
+++ week_02/김성용/login.js
@@ -5,3 +5,8 @@ function IDreturn(){
 function bordergreen(tag){
     tag.className = "gr";
   }
+function f_link(){
+    alert(IDreturn() + '님이 로그인하셨습니다.');
+    location.href = "login.html";
+  }
+
